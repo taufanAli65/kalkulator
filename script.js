e0aa6b29505f84dad980dd3edc3db9eed6494acb
Taufan Ali <126685412+taufanAli65@users.noreply.github.com>
2024-03-06 20:04:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 let cannotFirst = ['/', '+', '%', '*'];
-function hitung(symbol) {
+let index = 0;
+function hitung(symbol) { //to display into screen
     let calculate = document.querySelector(".calculate").innerHTML += symbol;
     if (cannotFirst.includes(calculate.charAt(0))) {del()}
+    else if (cannotFirst.includes(calculate.charAt(index)) && cannotFirst.includes(calculate.charAt(index-1))) {del()}
+    else if (calculate.charAt(index-1) == '-' && calculate.charAt(index) == '-') {del()};
+    index += 1;
 }
 
-function del() {
+function del() { //delete button function
     let user_input = document.querySelector(".calculate").innerText;
     user_input = user_input.slice(0, -1);
     let temporary = document.querySelector(".calculate").innerHTML = user_input;
+    index -= 1
 }
 
-function deleteAll() {
+function deleteAll() { //AC button function, which delete all calculating process
     let calculate = document.querySelector(".calculate").innerHTML = "";
     let answer = document.querySelector(".answer").innerHTML = "";
+    index = 0;
 }
 
-function process() {
+function process() { //to calculate the answer
     let calculate = document.querySelector(".calculate").innerText;
     let calculates = eval(calculate);
     if (calculates % 1 == 0) {

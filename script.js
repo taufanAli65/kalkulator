41904a36497b6c727bc1db17fe97a8ea75cf4b3a
Taufan Ali <126685412+taufanAli65@users.noreply.github.com>
2024-03-14 13:12:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let cannotFirst = ['/', '+', '%', '*'];
 let index = 0;
 function hitung(symbol) { //to display into screen
     let calculate = document.querySelector(".calculate").innerHTML += symbol;
-    if (cannotFirst.includes(calculate.charAt(0))) {del()}
-    else if (cannotFirst.includes(calculate.charAt(index)) && cannotFirst.includes(calculate.charAt(index-1))) {del()}
-    else if (calculate.charAt(index-1) == '-' && calculate.charAt(index) == '-') {del()};
+    if (cannotFirst.includes(calculate.charAt(0))) {del()} //users cannot using /,+,%, or * as the first click
+    else if (cannotFirst.includes(calculate.charAt(index)) && cannotFirst.includes(calculate.charAt(index-1))) {del()} //users cannot using using the same cannotFirst twice. Example : ++ or // or ** or %%
+    else if (calculate.charAt(index-2) == '-' && calculate.charAt(index-1) == '-') {
+        if (calculate.charAt(index) != '-') {null} else {del()}}; //users cannot using - 3x, example : 9---9
     index += 1;
 }
 
@@ -23,7 +24,11 @@ function deleteAll() { //AC button function, which delete all calculating proces
 
 function process() { //to calculate the answer
     let calculate = document.querySelector(".calculate").innerText;
-    let calculates = eval(calculate);
+    // if (calculate.includes('--')) {
+    //     calculate = calculate.replace('--', `-(-${calculate.charAt(index)})`);
+    //     alert(calculate);
+    // }; //error, I tried to handle case if users tried to minus number with minus number. Example : 9--9
+    calculates = eval(calculate);
     if (calculates % 1 == 0) {
         document.querySelector(".answer").innerHTML = calculates;
     } else {
